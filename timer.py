99fda6059147d480a2fce54aaded331d8750f42d
Whosiki <125159103+Whosiki@users.noreply.github.com>
2026-02-28 21:53:15 +0400

    timer1.py

--- timer.py
+++ timer.py
@@ -1,6 +1,11 @@
 import time
 secvalue=0
 tvalue=input("Input time hh:mm:ss ")
-cutpos1=tvalue.find(":")
-htosvalue=int(tvalue[0:cutpos1])*3600
-tvalue=tvalue[cutpos1 + 1:-1]
+cutpos=tvalue.find(":")
+htosvalue=int(tvalue[0:cutpos])*3600
+tvalue=tvalue[cutpos + 1:]
+cutpos=tvalue.find(":")
+mtosvalue=int(tvalue[0:cutpos])*60
+svalue=int(tvalue[cutpos + 1:])
+timerins=htosvalue+mtosvalue+svalue
+print(timerins)
