da72eb6f721751a6f45ac658feed4f27c8532599
Whosiki <spuychik@gmail.com>
2026-03-01 21:51:34 +0400

    upgrade 0.1

--- timer.py
+++ timer.py
@@ -1,13 +1,26 @@
 import time
 secvalue=0
-tvalue=input("Input time hh:mm:ss ")
-cutpos=tvalue.find(":")
-htosvalue=int(tvalue[0:cutpos])*3600
-tvalue=tvalue[cutpos + 1:]
-cutpos=tvalue.find(":")
-mtosvalue=int(tvalue[0:cutpos])*60
-svalue=int(tvalue[cutpos + 1:])
-timerins=htosvalue+mtosvalue+svalue
+state=True
+allowed_set = set("1234567890:")
+cutpos=-1
+htosvalue=0
+while state:
+    tvalue=input("Input time hh:mm:ss ")
+    dot_counted= tvalue.count(":")
+    outoforder= set(tvalue)-allowed_set
+    if not outoforder and dot_counted<=2:
+        state=False
+if dot_counted !=0:
+    if dot_counted == 2:
+        cutpos=tvalue.find(":")
+        htosvalue=int(tvalue[0:cutpos])*3600
+    tvalue=tvalue[cutpos + 1:]
+    cutpos=tvalue.find(":")
+    mtosvalue=int(tvalue[0:cutpos])*60
+    svalue=int(tvalue[cutpos + 1:])
+    timerins=htosvalue+mtosvalue+svalue
+
+
 while timerins >= 0:
     sec=timerins%60
     min=(timerins%3600 -sec)//60
